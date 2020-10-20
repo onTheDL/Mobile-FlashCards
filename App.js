f2cab74145cd0c8f2abfc23c02290e56c20cde55
onTheDL <daniel.laserna@protonmail.com>
2020-10-19 23:59:05 -0400

    Add utils folder and api file

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import DeckList from './components/DeckList'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <DeckList />
       <StatusBar style="auto" />
     </View>
   );
