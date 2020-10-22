0af899daaef0e40400a67b54ec1b5ab1bad1a314
onTheDL <daniel.laserna@protonmail.com>
2020-10-22 00:17:15 -0400

    Add reducer

---
+++ reducers/index.js
@@ -0,0 +1,41 @@
+import {
+  RECEIVE_DECKS,
+  ADD_DECK,
+  ADD_CARD,
+  DELETE_DECK,
+} from '../actions'
+
+export default function decks(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_DECKS :
+      return {
+
+      }
+    case ADD_DECK :
+      const { title } = action
+      return {
+        ...state,
+        [title]: {
+          title,
+          questions: []
+        },
+
+      }
+    case ADD_CARD :
+      const { deckId, card } = action
+      return {
+        ...state,
+        [deckId]: {
+          ...state.deckId,
+          questions: [...state[deckId].questions].concat(card),
+        }
+      }
+    case DELETE_DECK :
+      const { [action.deckId]: value, ...rest } = state
+      return {
+        ...rest
+      }
+    default :
+      return state
+  }
+}
