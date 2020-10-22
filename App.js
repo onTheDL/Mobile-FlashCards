5d8a0c5e4e641fd44aa8928bb9fb38c7c1446699
onTheDL <daniel.laserna@protonmail.com>
2020-10-22 00:39:25 -0400

    Install redux, react-redux packages, then connect store to Provider component

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
 import DeckList from './components/DeckList'
 import NewDeck from './components/NewDeck'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <DeckList /> */}
-
-      <DeckList />
-
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={createStore(reducer)} >
+      <View style={styles.container}>
+        {/* <DeckList /> */}
+        <DeckList />
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
+    
   );
 }
 
