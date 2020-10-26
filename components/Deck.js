90b9d8a3b0a18bfd3cb1b5e5d6f557dce0074946
onTheDL <daniel.laserna@protonmail.com>
2020-10-26 16:32:06 -0400

    Add fuurther styling to Deck

--- components/Deck.js
+++ components/Deck.js
@@ -8,7 +8,22 @@ function Deck({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-      <Text>{deckId}</Text>        
+      <View style={styles.textSubContainer}>
+        <Text style={{fontSize: 28, letterSpacing: 1.1}}>
+          {deckId}
+        </Text> 
+        <Text style={styles.subText}>x cards</Text> 
+      </View>
+
+      <View>
+        <TouchableOpacity style={styles.btn}>
+          <Text>Add Card</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.btn, styles.quizBtn]}>
+          <Text style={{color: 'white'}}>Start Quiz</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
@@ -19,10 +34,45 @@ const styles = StyleSheet.create({
     padding: 20,
     marginLeft: 10,
     marginRight: 10,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
+
+  textSubContainer: {
+    marginTop: 100,
+    alignItems: 'center',
+  },
+
+  subText: {
+    fontSize: 14, 
+    color: 'gray',
+    marginTop: 5,
+  },
+ 
+  buttons: {
+    // flex: 0.2,
+    // justifyContent: 'flex-end',
+    // height: 100,
+  },
+  btn: {
+    width: 150,
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 16,
+    //backgroundColor:'#438ae8',
+    // color: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 18,
+  },
+  btnText: {
+    fontSize: 16,
+  },
+
+  quizBtn: {
+    backgroundColor: 'black',
+  } 
   
-})
+})  
 
 export default Deck
